9785e1dd5a2e4679066fb3a2bbdf62545868b8bb
stauros5 <sbullntolas@gmail.com>
2024-12-31 12:01:21 +0200

    functions.h

--- functions.h
+++ functions.h
@@ -1,19 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-
-// Σταθερές
 #define PRODUCT_COUNT 20
 #define CLIENT_COUNT 5
 #define ORDERS_PER_CLIENT 10
 #define BUFFER_SIZE 256
 
-// Δομή για τα προϊόντα
 typedef struct {
     char description[50];
     float price;
@@ -23,10 +15,11 @@ typedef struct {
     char failed_clients[BUFFER_SIZE];
 } Product;
 
-// Δηλώσεις συναρτήσεων
 void initialize_catalog(Product catalog[]);
 void handle_order(int client_id, int product_id, Product catalog[], int client_pipe[2]);
 void generate_report(Product catalog[]);
+void client_process(int client_id, int server_pipe[2], int client_pipe[2]);
 
 #endif // FUNCTIONS_H
+
 
